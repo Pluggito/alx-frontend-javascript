4eaa383bfc71089c8058d2362684eaf08e48e76d
Pluggito <pluggitoshfl5050@gmail.com>
2025-09-14 14:15:11 +0100

    interface updated

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,7 @@ interface Teacher {
   [key: string]: any;                 
 }
 
+// Directors interface extending Teacher
 interface Directors extends Teacher {
   numberOfReports: number;            
 }
