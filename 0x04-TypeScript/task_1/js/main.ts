6b77446f144bf922858139dc7edc97f68919f9d7
Pluggito <pluggitoshfl5050@gmail.com>
2025-09-14 14:09:34 +0100

    Typescripts tasks

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,69 @@
+interface Teacher {
+  readonly firstName: string;         
+  readonly lastName: string;          
+  fullTimeEmployee: boolean;          
+  yearsOfExperience?: number;         
+  location: string;                   
+  [key: string]: any;                 
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;            
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage:
+const teacher1: Teacher = {
+  firstName: "Jane",
+  lastName: "Doe",
+  fullTimeEmployee: true,
+  location: "Lagos",
+  contract: false,            
+};
+
+const director1: Directors = {
+  firstName: 'James',
+  lastName: 'Daveson',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+const student: StudentClassInterface = new StudentClass("Alice", "Smith");
+
+console.log(director1);
+
+console.log(teacher1);
+
+console.log(printTeacher("John", "Doe")); // J. Doe
+
+console.log(student.displayName()); // Alice
+console.log(student.workOnHomework()); // Currently working
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,7 @@
+export type RowID = number | string;
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
