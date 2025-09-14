5d23ba8190bd25357ed01e15d500b15dcbf20f98
Pluggito <pluggitoshfl5050@gmail.com>
2025-09-14 21:59:59 +0100

    please don't let it be because fo spacing

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -35,7 +35,7 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number): Director | Teacher {
-  if ( salary < 500) {
+  if (salary < 500) {
     return new Teacher();
   }
   return new Director();
